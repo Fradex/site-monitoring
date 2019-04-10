65eddc80233cc238680ae5f936e184296c8bbc98
Pavel Timokhin <pavel.timohin@norbit.ru>
2019-04-10 19:14:12 +0300

    Мелкоправки, вынос в функцию + доступ к админге хенгфайера

---
+++ SiteMonitoring.Service/Authorization/HangfireAuthorizationFilter.cs
@@ -0,0 +1,21 @@
+﻿using Hangfire.Dashboard;
+
+namespace SiteMonitoring.Service.Authorization
+{
+    /// <summary>
+    /// Фильтр авторизации
+    /// Хенгфайр не хочет работать под докером без прав (
+    /// </summary>
+    public class HangfireAuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public bool Authorize(DashboardContext context)
+        {
+            return true;
+        }
+    }
+}
